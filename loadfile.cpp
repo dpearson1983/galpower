9b5fb78a15e8f3f655b4dd91a9d3269105ffc42c
Abhishek Jana <ajana@localhost.localdomain>
2019-06-10 23:45:16 -0500

    added error messege to break the infinite while loop while reading a file

--- loadfile.cpp
+++ loadfile.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 #include "loadfile.hpp"
 
 
@@ -9,35 +11,55 @@ void Loadfile::loadfile() {
     std::string filename;
     std::cout << "Enter the filename :\n";
     std::cin >> filename;
-    std::ifstream fin(filename);
-    std::string dummyLine;
-    std::getline(fin, dummyLine);
-    while (!fin.eof()) {
-        double3 gal;
-        fin >> gal.x >> gal.y >> gal.z;
-        if (!fin.eof()) {
-            galaxies.push_back(gal);
+    
+    
+    if (std::ifstream(filename)){
+        std::ifstream fin(filename);
+        std::string dummyLine;
+        std::getline(fin, dummyLine);
+        while (!fin.eof()) {
+            double3 gal;
+            fin >> gal.x >> gal.y >> gal.z;
+            if (!fin.eof()) {
+                galaxies.push_back(gal);
+            }
         }
+        fin.close();
     }
-    fin.close();
+    else {
+        std::stringstream errMsg;
+        errMsg << "Cannot open " << filename << "\n";
+        errMsg << "Please check that the file exists and is in the appropriate directory.\n";
+        throw std::runtime_error(errMsg.str());
+    }
 }    
 void Loadfile::loadfile(std::string filename) {
-    std::ifstream fin(filename);
-    std::string dummyLine;
-    std::getline(fin, dummyLine);
-    while (!fin.eof()) {
-        double3 gal;
-        fin >> gal.x >> gal.y >> gal.z;
-        if (!fin.eof()) {
-            galaxies.push_back(gal);
+    if (std::ifstream (filename)){
+        std::ifstream fin(filename);
+        std::string dummyLine;
+        std::getline(fin, dummyLine);
+        while (!fin.eof()) {
+            double3 gal;
+            fin >> gal.x >> gal.y >> gal.z;
+            if (!fin.eof()) {
+                galaxies.push_back(gal);
+            }
         }
+        fin.close();
     }
-    fin.close();
-}
+    else {
+        std::stringstream errMsg;
+        errMsg << "Cannot open " << filename << "\n";
+        errMsg << "Please check that the file exists and is in the appropriate directory.\n";
+        throw std::runtime_error(errMsg.str());
+    }
+}   
 
 int Loadfile::get_size(){
     return galaxies.size();
 }
+
+
 
 //void binNGP(std::vector <double3> &galaxies, std::vector )
 
