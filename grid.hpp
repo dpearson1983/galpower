bccc200d9d54010258569f541a302527a82881e6
David Pearson <dpearson@localhost.localdomain>
2019-06-19 16:13:45 -0500

    Outline of restructuring with parent and child classes

--- grid.hpp
+++ grid.hpp
@@ -1,49 +1,26 @@
 #ifndef _GRID_HPP_
 #define _GRID_HPP_
 
-#include <vector>
-#include <string>
-#include "structure.hpp"
-// #include "fft_helpers.hpp"
-
 class grid{
-    int4 N; // Stores grid dimensions and total number of grid points
-    double4 L; // Stores physical dimensions and volume
-    double3 Delta_r, r_min; // Delta_r stores grid spacing, r_min stores the minimum position allowed (0,0,0) by 
-                            // default
-    std::vector<double> k_x, k_y, k_z; // Storage for the frequecies
-    std::vector<double> F; // The grid itself
-    // We'll want to add storage for a fftw plan here at some point as well.
+    protected:
+        int4 N;
+        double4 L;
+        std::vector<double> F;
     
     public:
-        grid(); // Basic constructor
+        grid();
         
-        // Several different constructors for flexibility for the user
-        grid(int4 N, double4 L, double3 r_min = {0.0, 0.0, 0.0});
+        grid(int4 N, double4 L);
         
-        grid(int4 N, double4 L, std::vector<double3> &gals, double3 r_min = {0.0, 0.0, 0.0});
+        void init(int4 N, double4 L);
         
-        grid(int3 N, double3 L, double3 r_min = {0.0, 0.0, 0.0});
+        double get(int i, int j, int k);
         
-        grid(int3 N, double3 L, std::vector<double3> &gals, double3 r_min = {0.0, 0.0, 0.0});
+        int4 getSize();
         
-        void init(int4 N, double4 L, double3 r_min = {0.0, 0.0, 0.0});
+        double4 getDimension();
         
-        void init(int4 N, double4 L, std::vector<double3> &gals, double3 r_min = {0.0, 0.0, 0.0});
-        
-        void init(int3 N, double3 L, double3 r_min = {0.0, 0.0, 0.0});
-        
-        void init(int3 N, double3 L, std::vector<double3> &gals, double3 r_min = {0.0, 0.0, 0.0});
-        
-        void binNGP(std::vector<double3> &gals);
-        
-        // This could load the data from the file and then call the bin function, or we could leave this
-        // out and have a separate simple header file that loads the file, then we pass the galaxies to
-        // the class constructor or simply the binNGP function depending on the constructor we call
-        void load(std::string file);
-        void load();
-        int get_size();
-        
+        void rezeroGrid();
 };
 
 #endif
