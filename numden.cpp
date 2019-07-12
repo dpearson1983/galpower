c63c8b68878dc7a3d8f7195f1283e77d168699c7
Abhishek Jana <ajana@localhost.localdomain>
2019-07-12 00:54:02 -0500

    modified numden.cpp

--- numden.cpp
+++ numden.cpp
@@ -49,6 +49,7 @@ void numden::init(int4 N, double4 L, std::vector<double3> &gals, double3 r_min){
     // This line just calls the constructor for the vector, creating an empty vector and copying to
     // our data member.
     this->F = std::vector<double>(N.w);
+    numden::binNGP(gals);
 }
 
 
@@ -83,40 +84,12 @@ void numden::init(int3 N, double3 L, std::vector<double3> &gals, double3 r_min){
     // This line just calls the constructor for the vector, creating an empty vector and copying to
     // our data member.
     this->F = std::vector<double>(N.x*N.y*N.z);
+    numden::binNGP(gals);
 }
 
 // All other functions would need to be defined below here.
 
 
-// NOTE: This should be moved to a file_reader.cpp file which contains the definitions for everything in
-// the file_reader.hpp class definition, except the enum class part which doesn't need anything further. That
-// function would be called from main.cpp and then the galaxies would be passed to one of the other functions
-// here, either one of the constructors that accepts it as an argument, or to binNGP directly.
-void grid::loadfile(std::string filename) {
-    std::vector<double3> gals;
-    if (std::ifstream (filename)){
-        std::ifstream fin(filename);
-//         std::string dummyLine;
-//         std::getline(fin, dummyLine);
-        while (!fin.eof()) {
-            double3 gal;
-            fin >> gal.x >> gal.y >> gal.z;
-            if (!fin.eof()) {
-                gals.push_back(gal);
-            }
-        }
-        fin.close();
-        binNGP(gals); // NOTE: When moved, you won't call this function here. Instead loadfile will have a
-                      // return type of std::vector<double3>, and you will have to add return gals; at the 
-                      // end of the function.
-    }
-    else {
-        std::stringstream errMsg;
-        errMsg << "Cannot open " << filename << "\n";
-        errMsg << "Please check that the file exists and is in the appropriate directory.\n";
-        throw std::runtime_error(errMsg.str());
-    }
-}   
 
 // int grid::get_size(){
 //     return gals.size();
